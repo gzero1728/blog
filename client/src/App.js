90596e40ec1d49464a0ae9e20ca84c3c912c7ff6
gzero1728 <gzero1728@gmail.com>
2021-01-09 15:49:20 +0900

    bootstrap: header, footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 
+// Redux
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import store, { history } from "./store";
+import MyRouter from "./routes/Router";
+
+// 부트스트랩에서 기본 css를 불러옴 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/custom.scss";
+
+// React와 Redux store와 router연결
 function App() {
   return (
-    <div className="App">
-      Hello!
-    </div>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <MyRouter />
+      </ConnectedRouter>
+    </Provider>
   );
 }
 
