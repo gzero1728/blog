2553021049792daaf2fd23a686558721921531ce
gzero1728 <gzero1728@gmail.com>
2021-01-10 20:32:38 +0900

    client routes

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -2,14 +2,32 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import AppNavbar from "../components/AppNavbar";
+import { Container } from "reactstrap";
+import { Redirect, Route, Switch } from "react-router-dom";
+import PostCardList from "./normalRoute/PostCardList"
+import PostWrite from "./normalRoute/PostWrite";
+import PostDetail from "./normalRoute/PostDetail";
+import Search from "./normalRoute/Search";
+import CategoryResult from "./normalRoute/CategoryResult";
+import PostEdit from "./normalRoute/PostEdit";
 
 const MyRouter = () => (
-    <>
-        <AppNavbar />
-        <Header />
-        <h1>Hello Body</h1>
-        <Footer />
-    </>
+  <>
+    <AppNavbar />
+    <Header />
+    {/*라우트 컨테이너*/}
+    <Container id="main-body">
+      <Switch>
+        <Route path="/" exact component={PostCardList} />
+        <Route path="/post" exact component={PostWrite} />
+        <Route path="/post/:id" exact component={PostDetail} />
+        <Route path="/post/category/:categoryName" exact component={CategoryResult} />
+        <Route path="/search/:serachTerm" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </Container>
+    <Footer />
+  </>
 )
 
 export default MyRouter;
---
+++ client/src/routes/normalRoute/PostWrite.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PostWrite = () => {
+  return (
+    <h1>Post Write</h1>
+  )
+}
+
+export default PostWrite;
