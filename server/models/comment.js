05adad29252594825557fbf75da2d6029bc56e9a
gzero1728 <gzero1728@gmail.com>
2021-01-11 20:20:40 +0900

    server: api/post/comments GET, POST

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,26 +1,24 @@
 import mongoose from "mongoose";
 import moment from "moment";
 
-const commentSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: String,
-        default: moment().format("YYYY-MM-DD hh:mm:ss");
-    },
-    post: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "post"
-    },
-    creator: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
-    },
-    creatorName: {
-        type: Sting,
-    }
+const CommentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    default: moment().format("YYYY-MM-DD hh:mm:ss")
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "post"
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  creatorName: { type: String }
 });
 
 const Comment = mongoose.model("comment", CommentSchema);
--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -5,6 +5,7 @@ import auth from "../../middleware/auth";
 import Post from "../../models/post";
 import Category from "../../models/category";
 import User from "../../models/user"
+import Comment from "../../models/comment"
 
 const router = express.Router();
 
@@ -112,7 +113,6 @@ router.post("/", auth, uploadS3.none(), async (req, res, next) => {
 // @route      POST  api/post/:id
 // @desc       DetailPost
 // @access     Public
-
 router.get("/:id", async (req, res, next) => {
   try {
     const post = await Post.findById(req.params.id)
@@ -123,6 +123,57 @@ router.get("/:id", async (req, res, next) => {
     next(e)
   }
 })
+
+
+// [Comments Route]
+
+// @route    GET  api/post/comments
+// @desc     GET  All Comments
+// @access   public
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const comment = await Post.findById(req.params.id)
+      .populate({
+        path: "comment"
+      })
+    const result = comment.comments
+    console.log(result, "Comment load")
+    res.json(result)
+  } catch (e) {
+    console.log(e)
+  }
+});
+
+router.post("/:id/comments", async (req, res) => {
+  const newComment = await Comment.create({
+    contents: req.body.comments,
+    creator: req.body.userId,
+    creatorName: req.body.userName,
+    post: req.body.id,
+    date: moment().format("YYYY-MM-DD hh:mm:ss")
+  })
+  console.log(newComment, "newComment")
+  try {
+    await Post.findByIdAndUpdate(req.body.id, {
+      $push: {
+        comments: newComment._id
+      }
+    })
+    await User.findByIdAndUpdate(req.body.userId, {
+      $push: {
+        comments: {
+          post_id: req.body.id,
+          comment_id: newComment._id
+        }
+      }
+    })
+    res.json(newComment)
+  } catch (e) {
+    console.log(e)
+    next(e)
+  }
+})
+
 
 
 export default router;
